7205816ab33c41d1e410a8dd89228d6515cedd4b
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-05-28 19:43:59 +0100

    added /api/articles/:article_id along with relevant tests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   getAllTopics,
   getAllEndpoints,
+  getArticleById,
 } = require("./controllers/api.controllers");
 
 const app = express();
@@ -10,11 +11,22 @@ app.get("/api/topics", getAllTopics);
 
 app.get("/api", getAllEndpoints);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log("I run over here");
+
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
--- models/api.models.js
+++ models/api.models.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const db = require("../db/connection.js");
 const fs = require("fs/promises");
 
@@ -16,9 +17,30 @@ const fetchAllTopics = () => {
 const fetchAllEndpoints = () => {
   return fs.readFile("./endpoints.json").then((results) => {
     const parsedResults = JSON.parse(results);
-    // console.log(parsedResults, "results here");
     return parsedResults;
   });
 };
 
-module.exports = { fetchAllTopics, fetchAllEndpoints };
+const fetchArticleById = (id) => {
+  console.log(id, "id here at model");
+  if (id === undefined || !typeof id === "number") {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+  return db
+    .query(
+      `
+    SELECT * FROM articles
+    WHERE article_id = $1
+    `,
+      [id]
+    )
+    .then((results) => {
+      if (results.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Bad Request" });
+      } else {
+        return results.rows[0];
+      }
+    });
+};
+
+module.exports = { fetchAllTopics, fetchAllEndpoints, fetchArticleById };
